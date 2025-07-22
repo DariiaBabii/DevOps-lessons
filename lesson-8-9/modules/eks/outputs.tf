7395847d269add789ebc69a1243bfc7d17ec38a7
Dariia Babii <dariiababii02@gmail.com>
2025-07-22 18:18:32 +0300

    Add RDS module and update README with usage instructions

--- lesson-8-9/modules/eks/outputs.tf
+++ lesson-8-9/modules/eks/outputs.tf
@@ -1,11 +1,28 @@
+output "eks_node_role_arn" {
+  value = aws_iam_role.eks.arn
+}
+
+output "eks_cluster_ca_data" {
+  description = "EKS cluster CA cert"
+  value       = aws_eks_cluster.eks.certificate_authority[0].data
+}
+
 output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.eks.endpoint
 }
 
 output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.eks.name
 }
 
-output "eks_node_role_arn" {
-  value = aws_iam_role.eks.arn
+output "oidc_provider_arn" {
+  description = "OIDC provider ARN"
+  value       = aws_iam_openid_connect_provider.oidc.arn
+}
+
+output "oidc_provider_url" {
+  description = "OIDC provider URL"
+  value       = aws_iam_openid_connect_provider.oidc.url
 }
--- lesson-8-9/variables.tf
+++ lesson-8-9/variables.tf
@@ -1,5 +1,27 @@
 variable "cluster_name" {
-  description = "EKS cluster name for Jenkins and Helm provider"
+  description = "The name of the EKS cluster"
   type        = string
-  default     = "lesson-7-cluster"
+  default     = "eks-cluster-demo-nat"
+}
+
+variable "github_user" {
+  type        = string
+  description = "GitHub username for Jenkins credentials"
+}
+
+variable "github_pat" {
+  type        = string
+  description = "GitHub personal access token for Jenkins"
+  sensitive   = true
+}
+
+variable "github_repo_url" {
+  type        = string
+  description = "GitHub repo URL for Jenkins seed job"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones for RDS and VPC"
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 }
