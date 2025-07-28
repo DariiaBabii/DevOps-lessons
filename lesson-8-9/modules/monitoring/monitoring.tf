96b9c67feb8f7c5b59a4840bb079f1721564ed64
Dariia Babii <dariiababii02@gmail.com>
2025-07-28 18:59:29 +0300

    Finalize DevOps project with working EKS, RDS, Jenkins, Argo CD, and Monitoring

---
+++ lesson-8-9/modules/monitoring/monitoring.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "monitoring"
+  namespace        = var.namespace
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = var.chart_version
+  create_namespace = true
+
+  values = [file("${path.module}/values.yaml")]
+
+  depends_on = [var.helm_dependency]
+}
